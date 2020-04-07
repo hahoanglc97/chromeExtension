c06542a81d7fa654c972c188393a84eb0231298d
Hoang Hong Ha <hahh@magenest.com>
2020-04-07 22:45:32 +0700

    first extension in chrome

---
+++ FirstEx/contentScript.js
@@ -0,0 +1,5 @@
+var div = document.createElement('div');
+div.style = "position: fixed; bottom: 0; right: 0; z-index:9999";
+div.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/xCkcSL3zfJk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+ 
+document.body.appendChild(div);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chromeExtension
+link https://topdev.vn/blog/huong-dan-viet-mot-extension-tren-chrome/?fbclid=IwAR39jntoHg705DJ1P67gwyvHDSQlglvQoJmzMzS63-jcfE_qTazVRyoV5d8
